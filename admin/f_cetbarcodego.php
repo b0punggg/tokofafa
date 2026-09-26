cba01dc232d31953b5cf8b75bf49c900d3f995c1
b0punggg <argenta2705@gmail.com>
2026-09-26 10:27:20 +0700

    ukuran print barcode

--- admin/f_cetbarcodego.php
+++ admin/f_cetbarcodego.php
@@ -119,7 +119,7 @@
       $html2pdf = new Html2Pdf('P', array(80,3700), 'en', true, 'UTF-8', array(0, 0, 0, 0));
     }
     if($kertas=='70'){
-      $html2pdf = new Html2Pdf('P', array(70,3700), 'en', true, 'UTF-8', array(0, 0, 0, 0));
+      $html2pdf = new Html2Pdf('P', array(70,300), 'en', true, 'UTF-8', array(0, 0, 0, 0));
     } 
       $html2pdf->pdf->SetDisplayMode('fullpage');
       $html2pdf->writeHTML($content);
