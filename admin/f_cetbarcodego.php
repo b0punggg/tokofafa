36acb3e9bbd882a6a814fa31b2897c09ffdde0ca
b0punggg <argenta2705@gmail.com>
2026-09-26 13:51:54 +0700

    barcode -6

--- admin/f_cetbarcodego.php
+++ admin/f_cetbarcodego.php
@@ -50,7 +50,7 @@
       border-bottom: none;
     }
 </style>
-<page backtop="0mm" backbottom="0mm" backleft="4mm" backright="0mm">
+<page backtop="0mm" backbottom="0mm" backleft="0mm" backright="0mm">
   <table>    
      <?php 
       $i=0;$x=0;
